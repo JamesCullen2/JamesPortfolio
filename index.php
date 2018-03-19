fa14c00471e8de7cf6180105eaaca6d5e2b6adb2
JamesCullen2 <jamesnac@aol.com>
2018-03-19 16:04:54 +0000

    added text form for header url link

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 
 ?>
 
-<form method="post" action="account.php">
-    Type<select name="type">
+<form method="post" action="#">
+    Type: <select name="type">
         <option value="home">Home</option>
         <option value="about me">About Me</option>
         <option value="portfolio">Portfolio</option>
         <option value="home">Contact</option>
     </select>
-    <br>
+    <br><br>
+    <form method="post" action="#">
+        Link url: <input type="url" name="link url">
+    </form>
     <input type="submit">
 </form>
 
-Password<input type="text" name="password">
+<!--Password<input type="text" name="password">-->
