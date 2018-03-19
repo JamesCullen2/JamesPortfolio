2d5d816623616411c1d28db3050fc0667a3b1357
JamesCullen2 <jamesnac@aol.com>
2018-03-19 15:50:15 +0000

    added basic dropdown form for header

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<form method="post" action="account.php">
+    Type<select name="type">
+        <option value="home">Home</option>
+        <option value="about me">About Me</option>
+        <option value="portfolio">Portfolio</option>
+        <option value="home">Contact</option>
+    </select>
+    <br>
+    <input type="submit">
+</form>
+
+Password<input type="text" name="password">
