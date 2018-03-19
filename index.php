996c5e7bfb337bb05d81ac8279493796a11a83e0
JamesCullen2 <jamesnac@aol.com>
2018-03-19 16:36:12 +0000

    added input fields for about, portfolio, contac tand images sections

--- index.php
+++ index.php
@@ -1,19 +1,64 @@
 <?php
 
+session_start();
+
+if($_SESSION['submitted']) {
+    header('Location: admin.php');
+    exit;
+}
+
 ?>
 
-<form method="post" action="#">
+<h1>Portfolio Edit Page</h1>
+    <br><br>
+    <h2>Header Nav Bar Links</h2>
+    <br><br>
+<form method="post" action="admin.php">
     Type: <select name="type">
         <option value="home">Home</option>
         <option value="about me">About Me</option>
         <option value="portfolio">Portfolio</option>
         <option value="home">Contact</option>
     </select>
-    <br><br>
-    <form method="post" action="#">
-        Link url: <input type="url" name="link url">
+    Link url: <input type="url" name="link url">
     </form>
     <input type="submit">
 </form>
 
-<!--Password<input type="text" name="password">-->
+<h2>About Me</h2>
+<br><br>
+    <form method="post" action="admin.php">
+    Subtitle: <input type="text" name="subtitle">
+    Description: <input type="text" name="description">
+    </form>
+    <input type="submit">
+</form>
+
+<h2>Portfolio</h2>
+<br><br>
+<form method="post" action="admin.php">
+    Subtitle: <input type="text" name="subtitle">
+    Description: <input type="text" name="description">
+</form>
+<input type="submit">
+
+<h2>Contact</h2>
+<br><br>
+<form method="post" action="admin.php">
+    Type: <select name="type">
+        <option value="email">Email</option>
+        <option value="tel">Tel</option>
+        <option value="social">Social</option>
+    </select>
+    Description: <input type="text" name="description">
+</form>
+<input type="submit">
+
+<h2>Images</h2>
+<br><br>
+<form method="post" action="admin.php">
+    Subtitle: <input type="text" name="subtitle">
+    Description: <input type="text" name="description">
+</form>
+<input type="submit">
+
