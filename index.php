a8333fa17c5ab8ebb3843d650c7d539f3eafaa4e
JamesCullen2 <jamesnac@aol.com>
2018-03-20 13:58:04 +0000

    edited syntax after code review

--- index.php
+++ index.php
@@ -10,56 +10,75 @@
 
 <body>
     <h1>Portfolio Edit Page</h1>
-        <br><br>
+    <div>
         <h2>Header Nav Bar Links</h2>
         <br><br>
     <form method="post" action="admin.php">
-        Type: <select name="type">
+        <label for="type">Type</label>
+        <select name="type">
             <option value="home">Home</option>
             <option value="about me">About Me</option>
             <option value="portfolio">Portfolio</option>
             <option value="home">Contact</option>
         </select>
-        Link url: <input type="url" name="link url">
+        <label for="link">Link URL</label>
+        <input type="url" name="link url">
         </form>
         <input type="submit">
     </form>
+    </div>
 
+    <div>
     <h2>About Me</h2>
     <br><br>
         <form method="post" action="admin.php">
-        Subtitle: <input type="text" name="subtitle">
-        Description: <input type="text" name="description">
+            <label for="subtitle">Subtitle</label>
+            <input type="text" name="subtitle">
+            <label for="description">Description</label>
+            <input type="text" name="description">
         </form>
         <input type="submit">
     </form>
+    </div>
 
+    <div>
     <h2>Portfolio</h2>
     <br><br>
     <form method="post" action="admin.php">
-        Subtitle: <input type="text" name="subtitle">
-        Description: <input type="text" name="description">
+        <label for="subtitle">Subtitle</label>
+        <input type="text" name="subtitle">
+        <label for="description">Description</label>
+        <input type="text" name="description">
     </form>
     <input type="submit">
+    </div>
 
+    <div>
     <h2>Contact</h2>
     <br><br>
     <form method="post" action="admin.php">
-        Type: <select name="type">
+        <label for="type">Type</label>
+        <select name="type">
             <option value="email">Email</option>
             <option value="tel">Tel</option>
             <option value="social">Social</option>
         </select>
-        Description: <input type="text" name="description">
+        <label for="description">Description</label>
+        <input type="text" name="description">
     </form>
     <input type="submit">
+    </div>
 
+    <div>
     <h2>Images</h2>
     <br><br>
     <form method="post" action="admin.php">
-        Subtitle: <input type="text" name="subtitle">
-        Description: <input type="text" name="description">
+        <label for="subtitle">Subtitle</label>
+        <input type="text" name="subtitle">
+        <label for="description">Description</label>
+        <input type="text" name="description">
     </form>
     <input type="submit">
+    </div>
 </body>
 </html>
