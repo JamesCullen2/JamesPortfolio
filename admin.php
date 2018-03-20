848ac870e7bd00a342cc21df5309717f3487e3e1
JamesCullen2 <jamesnac@aol.com>
2018-03-20 16:20:09 +0000

    added db query for subtitle

--- admin.php
+++ admin.php
@@ -1,18 +1,14 @@
 <?php
 
 $db = new PDO('mysql:host = 127.0.0.1; dbname=portfolio', 'root');
+
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$query = $db->prepare("SELECT `subtitle` FROM `about_me`;");
 
+$query->execute();
 
-//$query = $db->prepare("INSERT INTO `adults` (`name`,`DOB`,`pet_name`,`gender`)
-//VALUES (:name, :dob, :pet_name, :gender);");
-//$email = 'charlie@charlie.com';
-//$name = 'Charlie';
-//$query->bindParam(':name', $name);
-//$query->bindParam(':email', $email);
-//
-//$query->execute();
+$subtitle = $query->fetchAll();
 
 ?>
 
