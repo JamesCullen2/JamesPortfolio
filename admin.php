7b6c130b912c58b7c476194095fc61ff03cdfe97
JamesCullen2 <jamesnac@aol.com>
2018-03-20 15:21:55 +0000

    added db varaible and pdo

--- admin.php
+++ admin.php
@@ -1,5 +1,19 @@
 <?php
 
+$db = new PDO('mysql:host = 127.0.0.1; dbname=portfolio', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+
+
+//$query = $db->prepare("INSERT INTO `adults` (`name`,`DOB`,`pet_name`,`gender`)
+//VALUES (:name, :dob, :pet_name, :gender);");
+//$email = 'charlie@charlie.com';
+//$name = 'Charlie';
+//$query->bindParam(':name', $name);
+//$query->bindParam(':email', $email);
+//
+//$query->execute();
+
 ?>
 
 
