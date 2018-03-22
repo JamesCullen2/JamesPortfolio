49a22c78722da6b105c438db2094e484c018ca25
JamesCullen2 <jamesnac@aol.com>
2018-03-22 07:22:47 +0000

    omitted unnecessary and unused html sections from codebase

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $subtitleArray = getAboutMeSubtitleFromDatabase($db);
 $textArray = getAboutMeDescriptionFromDatabase($db);
-//addAboutMeInfoToDatabase($_POST,$db);
+//$aboutMeArray = postAboutMeInfoToDatabase($db);
 
 ?>
 
@@ -18,24 +18,6 @@ $textArray = getAboutMeDescriptionFromDatabase($db);
 </head>
 
 <body>
-    <h1>Portfolio Edit Page</h1>
-    <div>
-        <h2>Header Nav Bar Links</h2>
-        <br><br>
-    <form method="post" action="admin.php">
-        <label for="type">Type</label>
-        <select name="type">
-            <option value="home">Home</option>
-            <option value="about me">About Me</option>
-            <option value="portfolio">Portfolio</option>
-            <option value="home">Contact</option>
-        </select>
-        <label for="link">Link URL</label>
-        <input type="url" name="link url">
-        <input type="submit">
-    </form>
-    </div>
-
     <div>
     <h2>About Me</h2>
     <h2>Edit</h2>
@@ -67,61 +49,16 @@ $textArray = getAboutMeDescriptionFromDatabase($db);
     </div>
 
     <div>
-    <h2>Portfolio</h2>
-    <br><br>
-    <form method="post" action="admin.php">
-        <label for="subtitle">Subtitle</label>
-        <input type="text" name="subtitle">
-        <label for="description">Description</label>
-        <input type="text" name="description">
-        <input type="submit">
-    </form>
-    </div>
-
-    <div>
-        <h2>Portfolio</h2>
-        <h2>Edit</h2>
-        <select name="subtitle">
-            <option value="websites">websites</option>
-            <option value="web apps">web apps</option>
-            <input type="submit" value="Get">
-        </select>
-        <br><br>
-        <form method="post" action="admin.php">
+        <h2>About Me</h2>
+        <h2>Delete</h2>
+        <form id="addAboutMe" method="post" action="admin.php">
             <label for="subtitle">Subtitle</label>
             <input type="text" name="subtitle">
-            <label for="description">Description</label>
-            <input type="text" name="description">
-            <input type="submit" value="Apply">
+            <br><br>
+            <textarea rows="25" cols="50" name="text" form="addAboutMe">
+            </textarea>
+            <input type="submit" name="submitNew" value="Add">
         </form>
-    </div>
-
-    <div>
-    <h2>Contact</h2>
-    <br><br>
-    <form method="post" action="admin.php">
-        <label for="type">Type</label>
-        <select name="type">
-            <option value="email">Email</option>
-            <option value="tel">Tel</option>
-            <option value="social">Social</option>
-        </select>
-        <label for="description">Description</label>
-        <input type="text" name="description">
-    <input type="submit">
-    </form>
-    </div>
-
-    <div>
-    <h2>Images</h2>
-    <br><br>
-    <form method="post" action="admin.php">
-        <label for="subtitle">Subtitle</label>
-        <input type="text" name="subtitle">
-        <label for="description">Description</label>
-        <input type="text" name="description">
-    <input type="submit">
-    </form>
     </div>
 </body>
 </html>
