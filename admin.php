42c787582f7da54a737f8ebba3f88c7e13db8978
JamesCullen2 <jamesnac@aol.com>
2018-03-22 16:52:47 +0000

    made semantic changes and omitted line breaks after review

--- admin.php
+++ admin.php
@@ -1,18 +1,19 @@
 <?php
-/**this function is to get subtitle column data from the about_me table in the database
+/**to get subtitle column data from the about_me table in the database
  *
  * @param $db array data to be selected from
  *
  * @return array of subtitle column values
  */
-function getAboutMeSubtitleFromDatabase($db) {
+function getAboutMeSubtitleFromDatabase($db) :array {
     $query = $db->prepare("SELECT `id`,`subtitle` FROM `about_me` WHERE `deleted` = 0;");
     $query->execute();
     return $query->fetchAll();
 }
-/**this function is to return the values of each subtitle in about_me section into a drop-down form
+
+/**to return the values of each subtitle in about_me section into a drop-down form
  *
- * @param $subtitleArray array of all values in subtitle field from about_me table
+ * @param $subtitleArray int of all values in subtitle field from about_me table
  *
  * @return string of all subtitles
  */
@@ -23,10 +24,10 @@ function listingSubtitles($subtitleArray) {
     }
     return $results;
 }
-/**this function is to get text column from about_me table in db
+
+/**to get text column from about_me table in db
  *
  * @param $db array data to be selected from
- *
  * @param $id mixed data to be selected from
  *
  * @return mixed text field from about_me table
@@ -39,7 +40,8 @@ function getAboutMeDescriptionFromDatabase($db, $id) {
     $query->execute();
     return $query->fetch();
 }
-/**this function is to return text field value of about_me section
+
+/**to return text field value of about_me section
  *
  * @param $textArray array of text field from about me table
  *
@@ -48,12 +50,11 @@ function getAboutMeDescriptionFromDatabase($db, $id) {
 function populateDescriptionForm($textArray) {
     return $textArray['text'];
 }
-/**this function is to edit current data with new data
+
+/**to edit current data with new data
  *
  * @param $db array data to be selected from
- *
  * @param $oldText string current data
- *
  * @param $newText string edited data
  *
  * @return string of new text to about_me table
@@ -64,12 +65,11 @@ function updateDescription($db, $oldText, $newText) {
     $query->bindParam(':newText', $newText);
     $query->execute();
 }
-/**this function is to insert new data into about_me table in db
+
+/**to insert new data into about_me table in db
  *
  * @param $db array of subtitles and text to be sent to db
- *
  * @param $newSubtitle string subtitle to be sent to db
- *
  * @param $newDescription string text description to be sent to db
  *
  * @return  string of subtitle and text values to populate new row of about_me table
@@ -81,7 +81,8 @@ function addAboutMe($db, $newSubtitle, $newDescription) {
     $query->bindParam(':newDescription', $newDescription);
     $query->execute();
 }
-/**this function is to delete data from about_me table
+
+/**to delete data from about_me table
  *
  * @param $db array of subtitle and text from db
  *
