f43d5e8d9d3bd7d8fd0735a7598a429a10681e3d
JamesCullen2 <jamesnac@aol.com>
2018-03-23 11:24:51 +0000

    edited function to display text in background section

--- webToDatabaseFunctions.php
+++ webToDatabaseFunctions.php
@@ -1,4 +1,6 @@
 <?php
+$db = new PDO('mysql:host = 127.0.0.1; dbname=portfolio', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 function getTextFromDB($db, $id) {
     $query = $db->prepare("SELECT `text` 
@@ -10,8 +12,19 @@ function getTextFromDB($db, $id) {
 }
 
 function displayText($getTextFromDB) {
-    return $getTextFromDB['text'];
+    $results = " ";
+    foreach ($getTextFromDB as $value) {
+        $results =  "<p" . $value['id'] . ">" . $value['text'] . "</p>";
+    }
+    return $results;
 }
 
 
+
+//function listingSubtitles($subtitleArray) {
+//    $results = " ";
+//    foreach ($subtitleArray as $value) {
+//        $results .=  "<option value='" . $value['id'] . "'>" . $value['subtitle'] . "</option>";
+//    }
+//    return $results;
 
