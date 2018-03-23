d4b4f1246a88ff14df62edccf1a4e0db7b6a4001
JamesCullen2 <jamesnac@aol.com>
2018-03-23 12:01:21 +0000

    added doc blocks and edited functionality

--- webToDatabaseFunctions.php
+++ webToDatabaseFunctions.php
@@ -1,7 +1,42 @@
 <?php
-$db = new PDO('mysql:host = 127.0.0.1; dbname=portfolio', 'root');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+/**to pull subtitles from about_me table in db
+ *
+ * @param $db array subtitle column from about_me table
+ * @param $id int id column from about_me table
+ *
+ * @return mixed subtitle info from db
+ */
+function getSubtitleFromDB($db, $id) {
+    $query = $db->prepare("SELECT `subtitle` 
+                            FROM `about_me`
+                            WHERE `id` = " . $id . "
+                            GROUP BY `subtitle`;");
+    $query->execute();
+    return $query->fetchAll();
+}
+
+/**to display each subtitle values as a string
+ *
+ * @param $getSubtitleFromDB array of id and subtitles
+ *
+ * @return string of selected subtitle
+ */
+function displaySubtitle($getSubtitleFromDB) {
+    $results = " ";
+    foreach ($getSubtitleFromDB as $value) {
+        $results =  "<p" . $value['id'] . ">" . $value['subtitle'] . "</p>";
+    }
+    return $results;
+}
+
+/**to pull text from about_me table in db
+ *
+ * @param $db array text column from about_me table
+ * @param $id int id column from about_me table
+ *
+ * @return mixed text info from db
+ */
 function getTextFromDB($db, $id) {
     $query = $db->prepare("SELECT `text` 
                             FROM `about_me`
@@ -11,20 +46,16 @@ function getTextFromDB($db, $id) {
     return $query->fetchAll();
 }
 
-function displayText($getTextFromDB) {
+/**to display each text description from about_me table
+ *
+ * @param $getTextFromDB array of id and text
+ *
+ * @return string of selected text
+ */
+function displayTextFromDB($getTextFromDB) {
     $results = " ";
     foreach ($getTextFromDB as $value) {
         $results =  "<p" . $value['id'] . ">" . $value['text'] . "</p>";
     }
     return $results;
 }
-
-
-
-//function listingSubtitles($subtitleArray) {
-//    $results = " ";
-//    foreach ($subtitleArray as $value) {
-//        $results .=  "<option value='" . $value['id'] . "'>" . $value['subtitle'] . "</option>";
-//    }
-//    return $results;
-
