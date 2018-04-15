2b24188d2434b0490a392d6d49b22148dd3e13e5
JamesCullen2 <jamesnac@aol.com>
2018-04-15 15:26:13 +0100

    auto scroll function for carousel

--- app/js/demo.js
+++ app/js/demo.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-        setting-name: setting-value
-});
-});
 
-$('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-});
+// $(document).ready(function(){
+//     $('.carousel').slick({
+//         slidesToShow: 3,
+//         dots:true,
+//         centerMode: true,
+//     })
+//     console.log('hello')
+// })
+
+
+$(document).ready(function() {
+    $('.autoplay').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+    })
+})
